563b247b677d77baa5e2a452071c3d316f9022ec
NestorCastro <nestornica10@gmail.com>
2023-08-04 21:56:14 -0600

    changed crud for task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -18,17 +18,18 @@ export class TasksController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tasksService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.tasksService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id, updateTaskDto);
+    return this.tasksService.update(id, updateTaskDto);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
+    return this.tasksService.remove(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task } from './entities/task.entity';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 
 @Injectable()
 export class TasksService {
@@ -12,24 +17,60 @@ export class TasksService {
   ) {}
 
   async create(createTaskDto: CreateTaskDto) {
-    const newTask = new this.taskModel(createTaskDto);
-    await newTask.save();
-    return newTask;
+    try {
+      const newTask = new this.taskModel(createTaskDto);
+      await newTask.save();
+      return newTask;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Task already exist');
+      }
+      throw error;
+    }
   }
 
   async findAll() {
-    this.taskModel.find();
+    const task = await this.taskModel.find();
+    return task;
   }
 
-  async findOne(id: number) {
-    return this.taskModel.findById(id);
+  async findOne(id: string) {
+    let task: Task;
+
+    if (!isNaN(+id)) {
+      task = await this.taskModel.findOne({ id });
+    }
+    //MongoId
+    if (!task && isValidObjectId(id)) task = await this.taskModel.findById(id);
+    if (!task)
+      throw new NotFoundException(
+        `Task with id, name or no "${id}" not found`,
+      );
+
+    return task;
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return this.taskModel.findByIdAndUpdate(id, updateTaskDto);
+  async update(id: string, updateTaskDto: UpdateTaskDto) {
+    const task = await this.findOne(id)
+    await this.taskModel.updateOne(updateTaskDto);
+    return task
   }
 
-  remove(id: number) {
-    return this.taskModel.findByIdAndDelete(id);
+  async remove(id: string) {
+    let task: Task;
+
+    if (!isNaN(+id)) {
+      task = await this.taskModel.findOne({ id });
+    }
+    //MongoId
+    if (!task && isValidObjectId(id)) task = await this.taskModel.findById(id);
+    if (!task) {
+      throw new NotFoundException(
+        `Task with id, name or no "${id}" not found`,
+      );
+    } else {
+      task = await this.taskModel.findByIdAndDelete(id)
+    }
   }
+  
 }
