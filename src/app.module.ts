435df00861db16e2358bc9261487f45bea4b869d
NestorCastro <nestornica10@gmail.com>
2023-08-04 20:18:32 -0600

    added mongodb with docker and added globalPipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/tasksdb'), TasksModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:25025/tasksdb'), TasksModule],
   controllers: [],
   providers: [],
 })
